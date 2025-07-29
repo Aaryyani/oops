dffdd58bf682cee30bbab4fa73ebd51f475a8077
Aaryyani <aaryyani.y46@gmail.com>
2025-07-29 14:15:35 +0530

    week-2

---
+++ week2.java
@@ -0,0 +1,15 @@
+package comp.example;
+
+public class week2 {
+
+	public static void main(String[] args) {
+		System.out.println(5+2); //7
+	    System.out.println(5-2); //3
+	    System.out.println(5*2); //10
+	    System.out.println(5/2); //2
+	    System.out.println(5%2); //1
+		
+
+	}
+
+}
