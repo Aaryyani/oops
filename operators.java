cf007f0c8fa4d8a524e976e4fbb441300a6189bf
Aaryyani <aaryyani.y46@gmail.com>
2025-07-29 14:21:15 +0530

    week-1(operatirs)

---
+++ operators.java
@@ -0,0 +1,16 @@
+package comp.example;
+
+public class operators extends week2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(5<2); 
+        System.out.println(5<=2); 
+   System.out.println(5>2); 
+   System.out.println(5>=2); 
+    System.out.println(5==2); 
+   System.out.println(5!=2);
+
+	}
+
+}
