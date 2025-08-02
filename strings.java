712418d18b4432cb3a91c78f3cf98a69cde58981
Aaryyani <aaryyani.y46@gmail.com>
2025-08-02 11:20:59 +0530

    strings

---
+++ strings.java
@@ -0,0 +1,14 @@
+package comp.example;
+
+public class strings {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Java\n"); 
+		System.out.println("Java"); 
+	    System.out.print("Python\n");
+	    System.out.print("LINUX/UNIX");
+
+	}
+
+}
