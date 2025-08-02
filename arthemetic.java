88e17e5f90522b41bb912e73da3d47f19f8c2a2b
Aaryyani <aaryyani.y46@gmail.com>
2025-08-02 10:24:54 +0530

    arthemeticoperaters

---
+++ arthemetic.java
@@ -0,0 +1,18 @@
+package comp.example;
+
+public class arthemetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=4,b=1;
+	    System.out.println(a);//4
+	    System.out.println(b);//1
+	    a = a * b;
+	        b = a / b;
+	    a = a / b;
+	    System.out.println(a);//1
+	    System.out.println(b);//4
+
+	}
+
+}
