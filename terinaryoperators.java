1739168b7470ead83298abe47ab3a7fdf8178d47
Aaryyani <aaryyani.y46@gmail.com>
2025-08-02 10:44:09 +0530

    terinaryoperators

---
+++ terinaryoperators.java
@@ -0,0 +1,12 @@
+package comp.example;
+
+public class terinaryoperators {
+
+	public static void main(String[] args) {
+		
+		 int x=40;
+		   System.out.println((x%2==0)?"even number":"odd number");
+
+	}
+
+}
