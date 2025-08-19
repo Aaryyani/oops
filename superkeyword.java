dc3f1036c3453146757d30f5ba5f4ff4a8523474
Aaryyani <aaryyani.y46@gmail.com>
2025-08-19 14:37:20 +0530

    super key word

---
+++ superkeyword.java
@@ -0,0 +1,38 @@
+package co2;
+class One 
+{
+   int i=10;
+
+void show()
+{
+ System.out.println("super class method: i=" +i );
+}
+}
+
+class Two extends One
+{
+  int i=20;
+
+void show()
+{
+System.out.println("sub class method: i=" +i);
+super.show();
+
+System.out.println("super i="+ super.i);
+}
+}
+
+
+
+
+public class superkeyword {
+
+	public static void main(String[] args) {
+		Two t=new Two();
+	    t.show();
+
+		
+
+	}
+
+}
