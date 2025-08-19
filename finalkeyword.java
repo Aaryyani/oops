1fa85012194e236d340261adcaf7cb36a968a24f
Aaryyani <aaryyani.y46@gmail.com>
2025-08-19 14:36:06 +0530

    final key word

---
+++ finalkeyword.java
@@ -0,0 +1,21 @@
+package co2;
+
+public class finalkeyword {
+
+	public static void main(String[] args) {
+		
+	        int x = 10;           
+	        final int y = 20;     
+
+	        System.out.println("x is: " + x);
+	        System.out.println("y is: " + y);
+
+	        x = 30;               
+	       
+	       // y = 40;            
+
+	       System.out.println("x is: " + x);
+	        System.out.println("y is: " + y);
+	}
+
+}
